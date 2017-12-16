7326a04964e9171586d0517478cd0e952e8028d9
Spyros Trigazis <spyridon.trigazis@cern.ch>
2017-12-17 00:38:56 +0100

    Update to kubernetes v1.9.0
    unset KUBELET_API_SERVER

--- kubernetes-kubelet/launch.sh
+++ kubernetes-kubelet/launch.sh
@@ -3,6 +3,8 @@
 source /etc/kubernetes/kubelet
 source /etc/kubernetes/config
 
+unset KUBELET_API_SERVER
+
 TEMP_KUBELET_ARGS='--cgroup-driver=systemd --cgroups-per-qos=false --enforce-node-allocatable='
 
 ARGS="$@ $TEMP_KUBELET_ARGS $KUBE_LOGTOSTDERR $KUBE_LOG_LEVEL $KUBELET_API_SERVER $KUBELET_ADDRESS $KUBELET_PORT $KUBELET_HOSTNAME $KUBE_ALLOW_PRIV $KUBELET_ARGS"
