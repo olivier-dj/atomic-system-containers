8c14a66a28302ea0b81e8b32cd8daae9325aea3f
olivier-dj <olivierdejonckere@gmail.com>
2018-03-14 13:50:12 +0100

    provide cuda

---
+++ docker-centos/cuda-path.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
